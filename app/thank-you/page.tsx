740c01229121796fdd8499bdd190af0feb73d453
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-08-14 18:26:05 +0500

    link

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -5,7 +5,7 @@ export default function ThankYouPage() {
   const router = useRouter()
 
   const handleJoinTelegram = () => {
-    window.open("https://t.me/+lGIkdK0juHUwNDU6", "_blank")
+    window.open("https://t.me/+HzuHstfa6x04MjAy", "_blank")
     // Redirect back to main page after a short delay
     setTimeout(() => {
       router.push("/")
