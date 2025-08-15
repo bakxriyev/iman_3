c415e226d7a8c0c10aebd1e9f6377f3cb59f4c63
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-08-15 16:43:13 +0500

    sds

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic"
 
-const LandingPageComponent = dynamic(() => import("@/components/landing-page"), {
+const LandingPageComponent = dynamic(() => import("../components/landing-page"), {
   ssr: false,
   loading: () => null,
 })
